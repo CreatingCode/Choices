bc80201486885d6bf41c622e263891fd783da029
Isaac Barron <ibdrawit@gmail.com>
2015-12-30 19:00:43 -0600

    Added bootstrap and menu to router

---
+++ choices/routes/menu.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ibarron on 12/30/15.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('menu', { title: 'Menu' });
+});
+
+module.exports = router;
