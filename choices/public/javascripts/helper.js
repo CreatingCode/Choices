0ce34c9d9f02e487a6830bfc888f707f48da8d0e
Isaac Barron <ibdrawit@gmail.com>
2015-12-31 18:08:59 -0600

    Added react to node_moduel dir

---
+++ choices/public/javascripts/helper.js
@@ -0,0 +1,3 @@
+/**
+ * Created by ibarron on 12/30/15.
+ */
--- choices/routes/menu.js
+++ choices/routes/menu.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
     res.render('menu',
         {
             title: 'Menu',
-            menuType: 'fixed'
+            menuType: 'sticky'
         });
 });
 
